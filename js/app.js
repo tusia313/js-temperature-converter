246c47c9d7a037cf239d600974bbe1098e0efb7e
Marta Łoskot <loskotmarta@gmail.com>
2024-04-08 18:47:37 +0200

    handling changes

--- js/app.js
+++ js/app.js
@@ -10,3 +10,17 @@ console.log('primary input', primaryInput.value)
 console.log('secondary input', secondaryInput.value)
 console.log('primary select', primarySelect.value)
 console.log('secondary select', secondarySelect.value)
+
+function update(e) {
+    let element = e.target.id
+    console.log(element,' update !')
+}
+
+function calculate() {
+    console.log('calculate !')
+}
+
+primaryInput.addEventListener('change', update)
+secondaryInput.addEventListener('change', update)
+primarySelect.addEventListener('change', update)
+secondarySelect.addEventListener('change', update)

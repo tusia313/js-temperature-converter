f8fd5c9a22eeccf4032da75d9b8293572648e88f
Marta Łoskot <loskotmarta@gmail.com>
2024-04-08 17:53:49 +0200

    Add input adn select value

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const primaryInput = document.querySelector('#primary-input')
+const secondaryInput = document.querySelector('#secondary-input')
+const primarySelect = document.querySelector('#primary-unit')
+const secondarySelect = document.querySelector('#secondary-unit')
+
+primaryInput.value = 0
+secondaryInput.value = 0
+
+console.log('primary input', primaryInput.value)
+console.log('secondary input', secondaryInput.value)
+console.log('primary select', primarySelect.value)
+console.log('secondary select', secondarySelect.value)

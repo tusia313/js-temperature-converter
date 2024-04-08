41ae9d1abd0339fba4aa9d218a3db3af90fc014c
Marta Łoskot <loskotmarta@gmail.com>
2024-04-08 21:01:54 +0200

    Math conversion done

--- js/app.js
+++ js/app.js
@@ -6,32 +6,43 @@ const secondarySelect = document.querySelector('#secondary-unit')
 primaryInput.value = 0
 secondaryInput.value = 0
 
-// console.log('primary input', primaryInput.value)
-// console.log('secondary input', secondaryInput.value)
-// console.log('primary select', primarySelect.value)
-// console.log('secondary select', secondarySelect.value)
-
 function update(e) {
     let elementId = e.target.id
     if (elementId === 'secondary-input') {
         primaryInput.value = calculate()
+
     } else {
         secondaryInput.value = calculate(primarySelect.value, secondarySelect.value, primaryInput.value)
     }
 }
 
 function calculate(firstTempUnit, secondTempUnit, temp) {
-    // console.log('calculate!', firstTempUnit, secondTempUnit, temp)
+    console.log('calculate!', firstTempUnit, secondTempUnit, temp)
     const combination = firstTempUnit + '-' + secondTempUnit
+    console.log(combination)
     let result
+
     switch (combination) {
-        case 'fahrenheit - celsius':
+        case 'fahrenheit-celsius':
             result = (Number(temp) - 32) * 5 / 9
             break
-        case 'celsius - fahrenheit':
-            result = (Number(temp) * 9/5) + 32
+        case 'celsius-fahrenheit':
+            result = (Number(temp) * 9 / 5) + 32
+            break
+        case 'kelvin-fahrenheit':
+            result = ((Number(temp) - 273.15) * 9) / 5 + 32
+            break
+        case 'kelvin-celcius':
+            result = Number(temp) - 273.15
+            break
+        case 'fahrenheit-kelvin':
+            result = ((Number(temp) - 32) * 5) / 9 + 273.15
+            break
+        case 'celcius-kelvin':
+            result = Number(temp) + 273.15
+            break
     }
-    return result
+    return result.toFixed(2)
 }
 
 primaryInput.addEventListener('change', update)
